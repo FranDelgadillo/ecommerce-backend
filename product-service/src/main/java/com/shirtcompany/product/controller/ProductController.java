73e2671be393c869372883ed5095f7d9593369fb
FranDelgadillo <frandelgadilloso@gmail.com>
2025-04-22 14:06:13 -0500

    Actualizando endpoints de ProductController

--- product-service/src/main/java/com/shirtcompany/product/controller/ProductController.java
+++ product-service/src/main/java/com/shirtcompany/product/controller/ProductController.java
@@ -4,7 +4,9 @@ import com.shirtcompany.product.dto.ProductResponseDTO;
 import com.shirtcompany.product.model.Product;
 import com.shirtcompany.product.service.ProductService;
 import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,23 +18,24 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/listAll")
+    @GetMapping
     public Flux<ProductResponseDTO> getAllProducts() {
         return productService.getAllProducts();
     }
 
-    @GetMapping("/getProductById/{id}")
+    @GetMapping("/{id}")
     public Mono<ProductResponseDTO> getProductById(
             @Parameter(description = "ID del producto") @PathVariable("id") Long id) {
         return productService.getProductById(id);
     }
 
-    @PostMapping("/create")
-    public Mono<Product> createProduct(@RequestBody Product product) {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Product> createProduct(@RequestBody @Valid Product product) {
         return productService.createProduct(product);
     }
 
-    @PutMapping("/updateProductById/{id}")
+    @PutMapping("/{id}")
     public Mono<ProductResponseDTO> updateProduct(
             @Parameter(description = "ID del producto a actualizar", required = true)
             @PathVariable("id") Long id,
@@ -40,7 +43,8 @@ public class ProductController {
         return productService.updateProduct(id, product);
     }
 
-    @DeleteMapping("/deleteProductById/{id}")
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> deleteProduct(
             @Parameter(description = "ID del producto a eliminar", required = true)
             @PathVariable("id") Long id) {
